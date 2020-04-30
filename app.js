73b698ce61975a207d00d74d4e569f79fc6b1bfd
banchakan <banchakan.m@gmail.com>
2020-04-30 20:41:46 +0700

    setting messages 2

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
