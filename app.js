f6391e261bccdf62b81255a7df3f0614af75bbc9
banchakan <banchakan.m@gmail.com>
2020-05-01 10:27:42 +0700

    show event message

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    reply(reply_token, req.body.events[0])
     res.sendStatus(200)
 })
 app.listen(port)
 
-function reply(reply_token) {
+function reply(reply_token, data_req) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {XdnwLY7GgF/eWd1Uy8nPCThmPtS9DfxU9wJU/MErEz3NwmCJ8TGX6Op35C4CczUTEHY1rlrTEzaiETLiqevZgdgiYk9Ds04TeYPEWXs2TqEqfKiGb3GqsOC+ovynf4mXVFFVbCLftNUC35+SgEuMdQdB04t89/1O/w1cDnyilFU=}'
@@ -22,14 +22,20 @@ function reply(reply_token) {
 
     let body = JSON.stringify({
         replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
-        }]
+        messages: [
+            {
+                type: 'text',
+                text: 'Hello'
+            },
+            {
+                type: 'text',
+                text: 'How are you?'
+            },
+            {
+                type: 'text',
+                text: data_req
+            }
+        ]
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
