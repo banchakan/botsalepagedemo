7bee67df92cf506587068778fb175257c137e6ef
banchakan <banchakan.m@gmail.com>
2020-05-02 10:20:59 +0700

    open method reply

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    //reply(reply_token, req.body.events[0].message)
+    reply(reply_token, req.body.events[0].message)
     //res.sendStatus(200)
     res.json(req.body.events)
 })
