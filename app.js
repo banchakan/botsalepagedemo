0cf8028cf2cc8099622b4cb60b3dde3ac9cfdeab
banchakan <banchakan.m@gmail.com>
2020-05-20 18:10:33 +0700

    get content image new version

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const axios = require('axios')
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
+const line = require('@line/bot-sdk');
 const app = express()
 const port = process.env.PORT || 4000
 
@@ -15,8 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.post('/webhook', (req, res) => {
+    //getContent('555', '11997242803572')
     if(req.body.events[0].message.type === 'image'){
-        getContent(req.body.events[0].replyToken, req.body.events[0].message.id)
+        getContent(req.body.events[0].message.id)
     }else{
         let model = {
             type: 'text',
@@ -43,22 +48,89 @@ function replyMessage(replyToken, jsonMessage){
     });
 }
 
-async  function getContent(replyToken , messageId){
+async function getContent(messageId){
     const config = {
         method: 'get',
-        url: `https://api.line.me/v2/bot/message/${messageId}/content`,
+        url: `https://api-data.line.me/v2/bot/message/${messageId}/content`,
         headers: {
+            'Content-Type': 'application/json',
             'Authorization': 'Bearer {XdnwLY7GgF/eWd1Uy8nPCThmPtS9DfxU9wJU/MErEz3NwmCJ8TGX6Op35C4CczUTEHY1rlrTEzaiETLiqevZgdgiYk9Ds04TeYPEWXs2TqEqfKiGb3GqsOC+ovynf4mXVFFVbCLftNUC35+SgEuMdQdB04t89/1O/w1cDnyilFU=}'
-        }
+        },
+        encoding: null
     }
-
-    let res = await axios(config)
-
-    let model = {
-        type: 'text',
-        text: `${res.data}`
-    }
-    replyMessage(replyToken, model)
-
-    console.log("DATA CONTENT >>> " , res);
+    let buffer = await axios(config)
+    
+    fs.writeFile('bank.jpg', buffer.data, function (err) {
+        if (err) return console.log(err);
+        console.log('buffer_data > bank.jpg');
+    });
 }
+
+
+// const client = new line.Client({
+    //     channelAccessToken: 'XdnwLY7GgF/eWd1Uy8nPCThmPtS9DfxU9wJU/MErEz3NwmCJ8TGX6Op35C4CczUTEHY1rlrTEzaiETLiqevZgdgiYk9Ds04TeYPEWXs2TqEqfKiGb3GqsOC+ovynf4mXVFFVbCLftNUC35+SgEuMdQdB04t89/1O/w1cDnyilFU='
+    // });
+    // let buffer = null
+    // client.getMessageContent(`${messageId}`)
+    // .then((stream) => {
+    //     stream.on('data', (chunk) => {
+    //         console.log("chunk => ", chunk)
+    //         // buffer = `${buffer}${chunk}`
+    //     });
+    //     stream.on('error', (err) => {
+    //         console.log("ERROR => ", err)
+    //     });
+    //     stream.on('finish', async (finish) =>{
+    //         console.log("finish => ", finish)
+    //         await fs.writeFileSync('test.jpg', finish);
+    //     })
+    // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+    // console.log('Headers => ', buffer.headers)
+    // console.log('Config => ', buffer.config)
+    // console.log('Request => ', buffer.request)
+    // console.log("DATA => ", buffer.data)
+
+    // let url = `https://api.line.me/v2/bot/message/${messageId}/content`
+
+    // let buffer = await request.get({
+    //     headers: {
+    //         'Authorization': 'Bearer {XdnwLY7GgF/eWd1Uy8nPCThmPtS9DfxU9wJU/MErEz3NwmCJ8TGX6Op35C4CczUTEHY1rlrTEzaiETLiqevZgdgiYk9Ds04TeYPEWXs2TqEqfKiGb3GqsOC+ovynf4mXVFFVbCLftNUC35+SgEuMdQdB04t89/1O/w1cDnyilFU=}'
+    //     },
+    //     uri: url,
+    //     //encoding: null // แก้ปัญหา binary ไม่สมบูรณ์จาก default encoding ที่เป็น utf-8
+    //   });
+
+    //   console.log(buffer)
+
+    // const tempLocalFile = path.join(os.tmpdir(), 'test.jpg');
+    // 
+
+    // var base64Data = buffer.data.replace(/^data:image\/png;base64,/, "");
+    // fs.writeFileSync("new.jpg", base64Data, 'base64', function(err) {
+    //     console.log(err);
+    // });
